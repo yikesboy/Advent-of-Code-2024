e00f9245ff23913b7e013532f1d8a8c3e4f41da9
JL <115737916+yikesboy@users.noreply.github.com>
2024-12-10 22:06:19 +0100

    Day 10 - Part Two

--- days/day10/main.go
+++ days/day10/main.go
@@ -14,12 +14,18 @@ func main() {
 		fmt.Fprintf(os.Stderr, "error parsing input file: %v\n", err)
 		return
 	}
-	num, err := partOne(hikingMap, trailStarts)
-	if err != nil {
+	if num, err := partOne(hikingMap, trailStarts); err != nil {
 		fmt.Fprintf(os.Stderr, "error part one: %v\n", err)
 		return
+	} else {
+		fmt.Printf("solution part one: %d\n", num)
 	}
-	fmt.Printf("solution part one: %d\n", num)
+	if num, err := partTwo(hikingMap, trailStarts); err != nil {
+		fmt.Fprintf(os.Stderr, "error part two: %v\n", err)
+		return
+	} else {
+		fmt.Printf("solution part two: %d\n", num)
+	}
 }
 
 func partOne(hikingMap [][]int, trailStarts []mapTile) (int, error) {
@@ -35,6 +41,52 @@ func partOne(hikingMap [][]int, trailStarts []mapTile) (int, error) {
 	}
 
 	return result, nil
+}
+
+func partTwo(hikingMap [][]int, trailStarts []mapTile) (int, error) {
+	result := 0
+
+	for _, start := range trailStarts {
+		paths := countPaths(hikingMap, start)
+		result += paths
+	}
+
+	return result, nil
+}
+
+func countPaths(hikinMap [][]int, start mapTile) int {
+	cache := make(map[string]int)
+	return dfsCountPaths(hikinMap, start, cache)
+}
+
+func dfsCountPaths(hikingMap [][]int, current mapTile, cache map[string]int) int {
+	key := fmt.Sprintf("%d,%d", current.x, current.y)
+
+	if count, exists := cache[key]; exists {
+		return count
+	}
+
+	if current.value == 9 {
+		return 1
+	}
+
+	totalPaths := 0
+
+	directions := [][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
+	for _, dir := range directions {
+		newX := current.x + dir[0]
+		newY := current.y + dir[1]
+
+		if newY >= 0 && newY < len(hikingMap) && newX >= 0 && newX < len(hikingMap[0]) {
+			if hikingMap[newY][newX] == current.value+1 {
+				totalPaths += dfsCountPaths(hikingMap, mapTile{newX, newY, hikingMap[newY][newX]}, cache)
+			}
+		}
+	}
+
+	cache[key] = totalPaths
+	return totalPaths
 }
 
 func dfs(hikingMap [][]int, current mapTile, visited map[string]bool, reachableNines *map[string]bool) {
